86b78a195c7b43e85ce9a180df31cfbd14075432
iTitus <iTitus@users.noreply.github.com>
2021-02-21 20:47:06 +0100

    Initial commit

---
+++ src/main/java/com/example/examplemod/NbtViewer.java
@@ -0,0 +1,16 @@
+package com.example.examplemod;
+
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
+
+@Mod("nbtviewer")
+public class NbtViewer {
+
+    public NbtViewer() {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST,
+                () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (remoteVersion, isNetwork) -> true));
+    }
+}
---
+++ src/main/java/com/example/examplemod/TextComponentHelper.java
@@ -0,0 +1,64 @@
+package com.example.examplemod;
+
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+
+import java.util.*;
+
+public final class TextComponentHelper {
+
+    private TextComponentHelper() {
+    }
+
+    private static List<ITextComponent> extract(ITextComponent text) {
+        if (text == null) {
+            return Collections.emptyList();
+        } else if (text.getSiblings().isEmpty()) {
+            return Collections.singletonList(text);
+        }
+
+        LinkedList<ITextComponent> list = new LinkedList<>();
+
+        ITextComponent copy = text.deepCopy();
+        for (Iterator<ITextComponent> it = copy.getSiblings().iterator(); it.hasNext(); ) {
+            ITextComponent next = it.next();
+            list.addAll(extract(next));
+            it.remove();
+        }
+
+        list.addFirst(copy);
+
+        return list;
+    }
+
+    public static List<ITextComponent> splitLines(ITextComponent text) {
+        if (text == null) {
+            return Collections.emptyList();
+        }
+
+        List<ITextComponent> lines = new ArrayList<>();
+
+        List<ITextComponent> extracted = extract(text);
+        if (!extracted.isEmpty()) {
+            ITextComponent current = new StringTextComponent("");
+            boolean hasSiblings = false;
+            for (ITextComponent t : extracted) {
+                if (t instanceof StringTextComponent && "\n".equals(((StringTextComponent) t).getText())) {
+                    lines.add(current);
+                    current = new StringTextComponent("");
+                    hasSiblings = false;
+                    continue;
+                }
+
+                current.getSiblings().add(t);
+                hasSiblings = true;
+            }
+
+            if (hasSiblings) {
+                lines.add(current);
+            }
+        }
+
+        return lines;
+    }
+}
